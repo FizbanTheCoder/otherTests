238f88ae609bdb8e4ebacbb216aa566620825994
LeszekJC <leszek.zielinski@jcommerce.pl>
2022-11-22 19:21:16 +0100

    Zmiana na kill pot

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'OtherTests';
-export const SITE_DESCRIPTION = 'I try find my way in software testing';
+export const SITE_TITLE = 'KillPotKitchen';
+export const SITE_DESCRIPTION = 'Moj blog fascynacji kulinarnych';
